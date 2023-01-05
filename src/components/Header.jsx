9008eee31f4fcdccad889e2fb7ca1b1ddcef5bc4
Thiago <thidurante@hotmail.com>
2023-01-05 16:52:45 -0300

    feat: adds initial react files to start portfolio projects

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
